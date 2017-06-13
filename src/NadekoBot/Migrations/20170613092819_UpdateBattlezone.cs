5919c0cc28da02a846ea67ca4e8dc7a491116bf4
Nielk1 <development@nielk1.com>
2017-06-13 04:34:09 -0500

    Added data migration

---
+++ src/NadekoBot/Migrations/20170613092819_UpdateBattlezone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NadekoBot.Migrations
+{
+    public partial class UpdateBattlezone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateAdded",
+                table: "BZ2GameProperties",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateAdded",
+                table: "BZ2GameProperties");
+        }
+    }
+}
