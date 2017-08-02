d71a49a3f77a55bb78b9ab2ab520c020c1457b97
Nielk1 <development@nielk1.com>
2017-08-01 21:59:47 -0500

    more trying to get this thing working

---
+++ src/NadekoBot/Modules/GamesList/GamesList.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.Commands;
+using NadekoBot.Attributes;
+using NadekoBot.Extensions;
+using NadekoBot.Services.GamesList;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadekoBot.Modules.GamesList
+{
+    public class GamesList : NadekoTopLevelModule
+    {
+        private readonly GamesListService _service;
+
+        public GamesList(GamesListService service)
+        {
+            _service = service;
+        }
+
+        [NadekoCommand, Usage, Description, Aliases]
+        public async Task Games(string type, [Remainder] string restOfLine = null)
+        {
+            if(!_service.IsValidGameType(type))
+            {
+                await ReplyErrorLocalized("invalid_gametype").ConfigureAwait(false);
+                return;
+            }
+
+            var channel = Context.Channel as ITextChannel;
+
+            await _service.GetGames(channel, type, restOfLine);
+        }
+    }
+}
---
+++ src/NadekoBot/Services/GamesList/GamesListService.cs
@@ -0,0 +1,70 @@
+﻿using Discord;
+using Discord.WebSocket;
+using NadekoBot.Extensions;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadekoBot.Services.GamesList
+{
+    public class GamesListService
+    {
+        private readonly DiscordShardedClient _client;
+        private readonly DbService _db;
+        private readonly ILocalization _localization;
+        private readonly NadekoStrings _strings;
+        //private readonly Timer checkWarTimer;
+
+        public ConcurrentDictionary<string, List<ITextChannel>> ActiveRequests { get; set; }
+
+        public GamesListService(DiscordShardedClient client, DbService db, ILocalization localization, NadekoStrings strings)
+        {
+            _client = client;
+            _db = db;
+            _localization = localization;
+            _strings = strings;
+
+            using (var uow = _db.UnitOfWork)
+            {
+                ActiveRequests = new ConcurrentDictionary<string, List<ITextChannel>>();
+            }
+        }
+
+        public bool IsValidGameType(string type)
+        {
+            switch(type.ToLowerInvariant())
+            {
+                case "help":
+                case "bz2":
+                case "bz98":
+                case "bz98r":
+                    return true;
+            }
+
+            return false;
+        }
+
+        public async Task GetGames(ITextChannel channel, string type, string restOfLine)
+        {
+            switch (type.ToLowerInvariant())
+            {
+                case "help":
+                    {
+                        var embed = new EmbedBuilder().WithOkColor()
+                                        .WithTitle("Games List")
+                                        .WithDescription( "<:game_icon_battlezone98redux:342134901975547916> `.games bz98`|`.games bz98r`\n"
+                                                        + "<:game_icon_battlezone2:342134902587785219> `.games bz2`");
+                        await channel.EmbedAsync(embed).ConfigureAwait(false);
+                    }
+                    break;
+                case "bz2":
+                    break;
+                case "bz98":
+                case "bz98r":
+                    break;
+            }
+        }
+    }
+}
