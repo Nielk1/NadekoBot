ceba44c091f6e04bfd2b717cbbd3a9f8bc44d4d7
Nielk1 <development@nielk1.com>
2019-10-24 20:53:12 -0500

    Added Inspiro command

---
+++ NadekoBot.Core/Modules/Searches/InspiroCommands.cs
@@ -0,0 +1,43 @@
+﻿using Discord;
+using Discord.Commands;
+using NadekoBot.Extensions;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NadekoBot.Common;
+using NadekoBot.Common.Attributes;
+
+namespace NadekoBot.Modules.Searches
+{
+    public partial class Searches
+    {
+        [Group]
+        public class InspiroCommands : NadekoSubmodule
+        {
+            private const string _xkcdUrl = "https://xkcd.com";
+            private readonly IHttpClientFactory _httpFactory;
+
+            public InspiroCommands(IHttpClientFactory factory)
+            {
+                _httpFactory = factory;
+            }
+
+            [NadekoCommand, Usage, Description, Aliases]
+            [Priority(0)]
+            public async Task Inspiro()
+            {
+                using (var http = _httpFactory.CreateClient())
+                {
+                    var response = await http.GetStringAsync("http://inspirobot.me/api?generate=true").ConfigureAwait(false);
+                    if (response == null || string.IsNullOrWhiteSpace(response))
+                        return;
+
+                    await ctx.Channel.EmbedAsync(new EmbedBuilder()
+                        .WithOkColor()
+                        .WithImageUrl(response)
+                        .WithDescription(response.Replace(@"https://generated.inspirobot.me/", @"http://inspirobot.me/share?iuid=")));
+                }
+            }
+        }
+    }
+}
