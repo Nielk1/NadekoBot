c5ccd25b7e0a90266cdf5a30e37f4d9acb73ccfe
Nielk1 <development@nielk1.com>
2019-11-11 02:08:02 -0600

    inspiro command adjustments to allow recalling inspiros by ID

--- NadekoBot.Core/Modules/Searches/InspiroCommands.cs
+++ NadekoBot.Core/Modules/Searches/InspiroCommands.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using NadekoBot.Common;
 using NadekoBot.Common.Attributes;
+using System.Text.RegularExpressions;
 
 namespace NadekoBot.Modules.Searches
 {
@@ -14,6 +15,7 @@ namespace NadekoBot.Modules.Searches
         [Group]
         public class InspiroCommands : NadekoSubmodule
         {
+            private Regex InspiroRegex = new Regex(@"((((\??iuid)?=)?(?'path'a))?\/)?(?'id'[a-zA-Z0-9]{10})(\.(?'ext'jpg)?)?$");
             private readonly IHttpClientFactory _httpFactory;
 
             public InspiroCommands(IHttpClientFactory factory)
@@ -23,13 +25,45 @@ namespace NadekoBot.Modules.Searches
             
             [NadekoCommand, Usage, Description, Aliases]
             [Priority(0)]
-            public async Task Inspiro()
+            public async Task Inspiro([Leftover] string query = null)
             {
+                if (string.IsNullOrWhiteSpace(query))
+                {
+                    query = null;
+                }
+                else
+                {
+                    Match m = InspiroRegex.Match(query);
+                    if (m.Groups["id"].Length > 0)
+                    {
+                        string path = m.Groups["path"]?.Value;
+                        if (string.IsNullOrWhiteSpace(path)) path = "a";
+                        string ext = m.Groups["ext"]?.Value;
+                        if (string.IsNullOrWhiteSpace(ext)) ext = "jpg";
+                        query = @"https://generated.inspirobot.me/" + path + "/" + m.Groups["id"].Value + "." + ext;
+                    }
+                    else
+                    {
+                        await ReplyErrorLocalizedAsync("inspiro_query_failed_parse").ConfigureAwait(false);
+                        return;
+                    }
+                }
+
                 using (ctx.Channel.EnterTypingState())
                 {
                     using (var http = _httpFactory.CreateClient())
                     {
-                        var response = await http.GetStringAsync("http://inspirobot.me/api?generate=true").ConfigureAwait(false);
+                        if (query != null)
+                        {
+                            HttpResponseMessage msg = await http.GetAsync(query, HttpCompletionOption.ResponseHeadersRead);
+                            if(msg.StatusCode != System.Net.HttpStatusCode.OK)
+                            {
+                                await ReplyErrorLocalizedAsync("inspiro_query_not_fount").ConfigureAwait(false);
+                                return;
+                            }
+                        }
+
+                        var response = query ?? await http.GetStringAsync("http://inspirobot.me/api?generate=true").ConfigureAwait(false);
                         if (response == null || string.IsNullOrWhiteSpace(response))
                             return;
 
